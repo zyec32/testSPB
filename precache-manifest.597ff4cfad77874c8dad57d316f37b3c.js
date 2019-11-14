b8baaea285e76fa4cb8c78be187b821ddece6a76
zyec32 <njkmrjpfzw@mail.ru>
2019-11-14 21:50:38 +0300

    Updates

---
+++ precache-manifest.597ff4cfad77874c8dad57d316f37b3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96082d160abc0de980c5865c7143997",
+    "url": "/testSPB/index.html"
+  },
+  {
+    "revision": "5e9725d7ffc03a3d832b",
+    "url": "/testSPB/static/css/main.84267fb5.chunk.css"
+  },
+  {
+    "revision": "9b97bbe87e66ebad97ce",
+    "url": "/testSPB/static/js/2.26d346ae.chunk.js"
+  },
+  {
+    "revision": "5e9725d7ffc03a3d832b",
+    "url": "/testSPB/static/js/main.a04c026e.chunk.js"
+  },
+  {
+    "revision": "93df586068aa6288ee4a",
+    "url": "/testSPB/static/js/runtime-main.89f08ca8.js"
+  }
+]);
