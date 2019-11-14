7016370a6763e278b93f3d2b792a279de8b1a55b
zyec32 <njkmrjpfzw@mail.ru>
2019-11-14 16:32:06 +0300

    First win at react

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import styled from 'styled-components';
 import Header from '../header';
 import Task from '../task';
@@ -16,10 +17,12 @@ const List = (props) => {
 
     return (
         <Column>
-            <Header name={props.name}/>
-            <Task name="task" description="lorem"/>
+            <Header name="6"/>
+            <Task name={props.tasks[0].text} description="lorem"/>
         </Column>
     )
 }
 
-export default List
+export default connect(state => ({
+    tasks: state.todos,
+}))(List)
