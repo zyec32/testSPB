831238f5612e8edfbed753f9cbcd94fdbfbb247a
zyec32 <njkmrjpfzw@mail.ru>
2019-11-14 11:44:05 +0300

    Add grids

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import List from './components/list';
+import styled from 'styled-components';
+
+const Table = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  background-color: #E5E5E6;
+  justify-items: center;
+`
 
 const App = () => {
   return (
-    <div style={styles.app}>
+    <Table>
       <List/>
-    </div>
+      <List/>
+      <List/>
+    </Table>
   );
 }
 
